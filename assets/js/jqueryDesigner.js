866b7e7caa399509bd9f2cdd974cd68cec28645b
n2ayalew <nattie623@gmail.com>
2016-06-07 00:12:02 -0400

    Add footer an header daaaawg

--- assets/js/jqueryDesigner.js
+++ assets/js/jqueryDesigner.js
@@ -1,4 +1,13 @@
-$(document).ready(
-	
-	);
+$(document).ready(function(){
+	//-------- OPEN POPUP----------------------
+    $('[data-popup=popup-1]').fadeIn(350);
+	//----- CLOSE POPUP--------------------
+    $('[data-popup-close]').on('click', function(e)  {
+        $('[data-popup=popup-1]').fadeOut(350);
+ 
+        e.preventDefault();
+    });
+
+
+});
 
