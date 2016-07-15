588223658c26960820febf6562e3cc0738bebb6c
n2ayalew <nattie623@gmail.com>
2016-07-15 11:12:00 -0400

    Add fabricJS to project

--- assets/js/jqueryDesigner.js
+++ assets/js/jqueryDesigner.js
@@ -1,9 +1,15 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+/*
+	We need to convert to EaselJS
+*/ 
+//var canvas = document.getElementById("canvas");
+
+var cnvs = new fabric.Canvas('canvas');
+var ctx = cnvs.getContext("2d");
 
 var prodxmin,prodymin,prodxmax,prodymax,
     textDisplayed, designText, rotateCnt, imgLoaded, mouseDown, textx, texty, dragok, textMetircObj, fontSize, font,
-    scalex, scaley, designx,designy,designw,designh, design, designLoaded, ddragok; 
+    scalex, scaley, designx,designy,designw,designh, design, designLoaded, ddragok, outOfFrame, moffsetx, moffsety, imgoffset,
+    prevx, prevy; 
 
 /*
 	- Array of image paths for each product ... there is only three for now
@@ -12,6 +18,15 @@ var prodxmin,prodymin,prodxmax,prodymax,
 var productImageArray, currentProdIndex, loadedProdImage;
 
 function init() {
+
+	var rect = new fabric.Rect({
+  		left: 100,
+  		top: 100,
+  		fill: 'red',
+  		width: 20,
+  		height: 20
+	});
+	cnvs.add(rect);
 	startx = 0.25 * canvas.width;
 	starty = 0.25 * canvas.height;
 	scalex = 0.4;
@@ -53,10 +68,10 @@ function moveText(e){
 }
 
 function moveDesign(e) {
-	if (ddragok) {
+	if (ddragok && !outOfFrame) {
 		var cord = getMousePos(canvas, e);
-		designx = cord.x;
-		designy = cord.y;
+		designx = cord.x;// - prevx;
+		designy = cord.y;// - prevy;
 	}
 }
 
@@ -64,21 +79,23 @@ function handleFiles(files) {
 	var file = files[0];
 	// Send file to server
 	// append parent directory for testing purposes
-	cordnsole.log(file.name);
-	//design.src =  '../../Pictures/' +  file.name;
+	console.log(file.name);
+	design.src =  '../../Pictures/' +  file.name;
 	designLoaded = true;
+	
 	
 }
 $(document).ready(function (){
+	init();
 	//-------- OPEN POPUP----------------------
-    $('[data-popup=popup-1]').fadeIn(350);
+    //$('[data-popup=popup-1]').fadeIn(350);
 	//----- CLOSE POPUP--------------------
-    $('[data-popup-close]').on('click', function(e)  {
-        $('[data-popup=popup-1]').fadeOut(350);
+    //$('[data-popup-close]').on('click', function(e)  {
+        //$('[data-popup=popup-1]').fadeOut(350);
  
-        e.preventDefault();
+        //e.preventDefault();
         $('#canvas').css('background', 'rgba(0,0,0,0)');
-    });
+    //});
 
 	$('.product').click(function (){
 		rotateCnt = 0;
@@ -93,8 +110,9 @@ $(document).ready(function (){
 		prodxmax = imgw + startx - 0.15 * imgw;
 		prodymax = imgh + starty - 0.15 * imgh;
 		imgLoaded = true;
-		designw = scalex * loadedProdImage.width * 0.3;
-		designh = scaley * loadedProdImage.height * 0.3;
+		
+		designw = scalex  * 0.3 * loadedProdImage.width;
+		designh = scaley  * 0.3 * loadedProdImage.height;
 		//loadedProdImage.src = productImageArray[currentProdIndex][rotateCnt];
 		// we should use the load event listener when we are loading from server
 
@@ -120,14 +138,29 @@ $(document).ready(function (){
 
 
 	$('#canvas').mousemove(function (e){
-		//console.log('x pos:' + e.pageX + ' y pos:' + e.pageY + 'texty:' + texty + ' textx:' + textx + ' yOffSet:' + canvas.offsetTop);
 		var cord = getMousePos(canvas, e);
+
 		//console.log('x pos: ' + cord.x + 'y pos: ' + cord.y);
+
+
+		var dx = cord.x - prevx;
+		var dy = cord.y - prevy;
+		
+		if (designx < cord.x && cord.x < designx + designw && designy < cord.y && cord.y < designy + designh) {
+			$('#canvas').css('cursor', 'pointer');
+		} else {
+			$('#canvas').css('cursor','auto');
+			ddragok = false;
+		}
+
+		outOfFrame = false;
+		
+		//console.log('adfadsf');
 		if (e.which == 1 &&
 				textx < cord.x && cord.x < textx + textMetircObj.width
-				&& texty < cord.y && cord.y < texty + fontSize) {
-			textx = cord.x;
-			texty = cord.y;
+				&& texty < cord.y && cord.y < texty + fontSize && !outOfFrame) {
+			textx = (cord.x - prevx) + textx 
+			texty = (cord.y - prevy) + texty;
 			dragok = true;
 			canvas.onmousemove = moveText;
 			//console.log('x pos:' + textx + 'y pos:' + texty);
@@ -136,40 +169,77 @@ $(document).ready(function (){
 		if (e.which == 1 &&
 				designx < cord.x && cord.x < designx + designw
 				&& designy < cord.y && cord.y < designy + designh) {
-			designx = cord.x;
-			designy = cord.y;
-			ddragok = true;
-			canvas.onmousemove = moveDesign;
+			
+			if ( (designx + dx) < prodxmin || cord.x < prodxmin) {
+				designx = prodxmin;
+				ddragok = false;
+				canvas.onmousemove = null;
+				outOfFrame = true;
+			} 
+
+			if ( (designx + designw) + dx > prodxmax || cord.x > prodxmax) {
+				designx = prodxmax - designw;
+				ddragok = false;
+				canvas.onmousemove = null;
+				outOfFrame = true;
+			}
+
+			if ( (designy + dy) < prodymin || cord.y < prodymin) {
+				designy = prodymin;
+				ddragok = false;
+				canvas.onmousemove = null;
+				outOfFrame = true;
+			}
+
+			if ( (designy + designh) + dy > prodymax || cord.y > prodymax) {
+				designy = prodymax - designh;
+				ddragok = false;
+				canvas.onmousemove = null;
+				outOfFrame = true;
+			}
+
+
+			if (!outOfFrame) {
+				designx = (cord.x - prevx) + designx;
+				designy = (cord.y - prevy) + designy;
+				ddragok = true;
+				canvas.onmousemove = moveDesign;
+			}
 		}
+
+		prevx = cord.x;
+		prevy = cord.y;
 		
 	});
 	$('#canvas').mouseup(function(e){
+		//outOfFrame = false;
 		dragok = false;
 		ddragok = false;
 		canvas.onmousemove = null;
 	});
 });
 
-function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-	if ( imgLoaded && currentProdIndex >= 0) {
-		var imgw = loadedProdImage.width * scalex;
-		var imgh = loadedProdImage.height * scaley;
-		ctx.drawImage(loadedProdImage, startx , starty, imgw, imgh);
-		ctx.beginPath();
-		ctx.rect(prodxmin, prodymin,prodxmax - prodxmin,prodymax - prodymin);
-		ctx.stroke();
-	}
-	if (textDisplayed) {
-		ctx.fillText(designText, textx, texty);
-	}
-	if(designLoaded){
-		ctx.drawImage(design, designx, designy, designw, designh)
-	}
+// function draw() {
 	
-	window.requestAnimationFrame(draw);
-}
+// 	//ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-init();
+// 	if ( imgLoaded && currentProdIndex >= 0) {
+// 		var imgw = loadedProdImage.width * scalex;
+// 		var imgh = loadedProdImage.height * scaley;
+// 		ctx.drawImage(loadedProdImage, startx , starty, imgw, imgh);
+// 		ctx.beginPath();
+// 		ctx.rect(prodxmin, prodymin,prodxmax - prodxmin,prodymax - prodymin);
+// 		ctx.stroke();
+// 	}
+// 	if (textDisplayed) {
+// 			ctx.fillText(designText, textx, texty);
+// 	}
+// 	if(designLoaded){
+// 		ctx.drawImage(design, designx, designy, designw, designh)
+// 	}
+	
+// 	window.requestAnimationFrame(draw);
+// }
+
+
 
